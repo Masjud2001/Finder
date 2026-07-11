798c7f291248a2ee3504acdbe7c79a3c9e6e3039
Masjud2001 <jitislam340@gmail.com>
2026-07-10 21:58:12 -0400

    Create Features.jsx

---
+++ Features.jsx
@@ -0,0 +1,61 @@
+function Features() {
+
+return(
+
+<section className="features">
+
+<div className="card">
+
+<h2>
+
+❤️ Compatibility
+
+</h2>
+
+<p>
+
+Find people based on values instead of endless swiping.
+
+</p>
+
+</div>
+
+<div className="card">
+
+<h2>
+
+🔒 Privacy
+
+</h2>
+
+<p>
+
+Control how notifications appear and protect your privacy.
+
+</p>
+
+</div>
+
+<div className="card">
+
+<h2>
+
+🤖 AI Matching
+
+</h2>
+
+<p>
+
+Receive intelligent recommendations based on personality.
+
+</p>
+
+</div>
+
+</section>
+
+)
+
+}
+
+export default Features;
