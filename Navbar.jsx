fc92453ad1796ff6c0a1970d4ffad63ac0db994b
Masjud2001 <jitislam340@gmail.com>
2026-07-10 21:57:27 -0400

    Create Navbar.jsx

---
+++ Navbar.jsx
@@ -0,0 +1,29 @@
+function Navbar() {
+  return (
+    <nav className="navbar">
+
+      <div className="logo">
+        ❤️ SoulSync
+      </div>
+
+      <ul>
+
+        <li>Discover</li>
+
+        <li>Compatibility</li>
+
+        <li>Safety</li>
+
+        <li>Premium</li>
+
+      </ul>
+
+      <button className="loginBtn">
+        Sign In
+      </button>
+
+    </nav>
+  );
+}
+
+export default Navbar;
